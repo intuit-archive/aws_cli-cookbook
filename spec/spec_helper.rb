ccd35b3afae7189f8715a9741cabe46fd9ae92b8
Grant Hoffman <grantleehoffman@gmail.com>
2014-04-03 16:05:44 -0700

    updated naming

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'yaml'
 
-libs = File.expand_path("../../libraries/route53/", __FILE__)
+libs = File.expand_path("../../libraries/", __FILE__)
 
 require "#{libs}/change_batch"
 require "#{libs}/update_cname"
