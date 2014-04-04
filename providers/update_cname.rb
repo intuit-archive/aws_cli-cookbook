2953860487a123b57e1e44f6a86db4f1ea7bbdcb
Grant Hoffman <grantleehoffman@gmail.com>
2014-04-03 17:00:49 -0700

    proper syntax on provider

--- providers/update_cname.rb
+++ providers/update_cname.rb
@@ -8,14 +8,14 @@ action :update_cname do
   region          = new_resource.region
   hosted_zone_id  = new_resource.hosted_zone_id
   
-  cmd = UpdateCname.new.build_command hostname:        = hostname,
-                                      record:          = record,
-                                      domain:          = domain,
-                                      record_type:     = record_type,
-                                      ttl:             = ttl,
-                                      change_type:     = change_type,
-                                      region:          = region, 
-                                      hosted_zone_id:  = hosted_zone_id
+  cmd = UpdateCname.new.build_command hostname:        hostname,
+                                      record:          record,
+                                      domain:          domain,
+                                      record_type:     record_type,
+                                      ttl:             ttl,
+                                      change_type:     change_type,
+                                      region:          region, 
+                                      hosted_zone_id:  hosted_zone_id
   
   execute "Updating Route53 Cname Entry: #{record}.#{domain} = #{hostname}" do
     command cmd
