71073340cd02159e777177dca7962c20a0c401c9
Grant Hoffman <grantleehoffman@gmail.com>
2014-04-04 11:24:06 -0700

    making record the name attribute

--- resources/update_cname.rb
+++ resources/update_cname.rb
@@ -1,8 +1,8 @@
 actions :update_cname
 default_action :update_cname
 
-attribute :hostname, :kind_of => String, :name_attribute => true
-attribute :record, :kind_of => String
+attribute :record, :kind_of => String, :name_attribute => true
+attribute :hostname, :kind_of => String
 attribute :domain, :kind_of => String
 attribute :record_type, :kind_of => String, :default => "CNAME"
 attribute :ttl, :kind_of => Integer, :default => 60
